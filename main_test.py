8e9fc32f8e7146f3511e515788fd9d8e10d7d5e7
Timothé Jobin <timothe.jobin@epfl.ch>
2026-03-16 15:54:13 +0100

    add momentary version of main file

--- main_test.py
+++ main_test.py
@@ -2,18 +2,19 @@ import numpy as np
 from lanczos_method import optimize_lanczos_step
 from moments_estimator import estimate_hamiltonian_moments
 from tenpy.algorithms import dmrg
-from tenpy.models.tf_ising import TFIChain
 from tenpy.networks.mps import MPS
-from tenpy.models.spins import SpinModel
 from tenpy.models.hubbard import BoseHubbardModel
 from _plot import plot_E_vs_chi
-from modified_DMRG import ExactStateDMRGEngine
-from tenpy.algorithms.exact_diag import ExactDiag
+from utils import get_exact_psi_and_E, EXACT_ENERGIES
 
+Lx, Ly = 4, 3
+N = Lx * Ly  # Half-filling for bosons
+
+## Define the model parameters for a Lx by Ly cylinder of the Bose-Hubbard model
 model_params = {
         "lattice": "Square",
-        "Lx": 5,             # Number of sites along the cylinder length
-        "Ly": 2,             # Circumference of the cylinder (number of sites)
+        "Lx": Lx,             # Number of sites along the cylinder length
+        "Ly": Ly,             # Circumference of the cylinder (number of sites)
         "bc_MPS": "finite",   # Finite MPS (open boundaries for the 1D path)
         "bc_y": "cylinder",   # Periodic boundary conditions in the y-direction
         "bc_x": "open",       # Open boundary conditions in the x-direction
@@ -27,18 +28,25 @@ model_params = {
 model = BoseHubbardModel(model_params)
 H_mpo = model.H_MPO
 
+## Get exact ground state energy
+if (Lx, Ly) in EXACT_ENERGIES:
+    E_exact = EXACT_ENERGIES[(Lx, Ly)]
+else:
+    _, E_exact = get_exact_psi_and_E(model, charge_sector=[N], max_size=1e15)
+
+
 ## Create an initial MPS in the product state |up up up ...>
 product_state = [1] * model.lat.N_sites
 psi = MPS.from_product_state(model.lat.mps_sites(), product_state, bc=model.lat.bc_MPS)
 
-## Range of max bond dimension chi values to test
-chi_maxs = [16]#, 16, 32, 64, 128, 256, 512, 1024]
-
 ## Lists to store raw DMRG energies and Lanczos-optimized energies for each chi value
-E = []
-E_alpha = []
+E_dmrg = []
+E_lanczos = []
 
+## Range of max bond dimension chi values to test
+chi_maxs = [12, 16, 20, 24, 28, 32]
 
+## Run DMRG for each bond-dimension chi
 for chi_max in chi_maxs:
     dmrg_params = {
             'mixer': True,           # Adds a density matrix perturbation to escape local minima
@@ -48,22 +56,24 @@ for chi_max in chi_maxs:
                 'svd_min': 1.0e-10   # Discarded weight / singular value cutoff
             },
             'verbose': 1,            # Print progress output
-            # 'max_sweeps': 10,         # Maximum number of DMRG sweeps
             'combine': True,          # Combine local tensors for the 2-site update
-            'active_sites': 1
+            'active_sites': 2
         }
 
     info = dmrg.run(psi, model, dmrg_params)
-    h1, psii = info['E'], info['psi_i']
+    Ei, psii = info['E'], info['psi_i']
     trunc_params = dmrg_params['trunc_params']
 
+    ## Lanczos step using the estimated moments from perfect sampling
     h1_est, h2_est, h3_est = estimate_hamiltonian_moments(
         psii,
         H_mpo,
-        N_s=2048,
+        N_s=256,
+        atol=2e-2,
         filename=f'moments_chi{chi_max}',
         seed=42
         )
+    
     _, alpha_star = optimize_lanczos_step(h1_est, h2_est, h3_est)
 
     phi_1 = psii.copy()
@@ -72,30 +82,22 @@ for chi_max in chi_maxs:
     psi_alpha = psii.add(other=phi_1, alpha=1.0, beta=alpha_star)
     E_alpha = np.real(H_mpo.expectation_value(psi=psi_alpha))
 
+    E_lanczos.append(E_alpha)
+    E_dmrg.append(Ei)
+    # Use variance as an indicator of how close we are to the true ground state
     print(f'Variance {H_mpo.variance(psii):.10e}')
 
-    # Exact energy from exact diagonalization for comparison
-    # exact_diag = ExactDiag(model, max_size=1e12)
-    # exact_diag.build_full_H_from_mpo()
-    # exact_diag.full_diagonalization()
-    # exact_out = exact_diag.groundstate()  # Ground state energy
-    # E_exact, psi_exact = exact_out[0], exact_out[1]
+    # Compare the raw DMRG energy and the Lanczos-optimized energy
+    print(f'Exact energy : {E_exact:.16f}')
+    print(f'DMRG energy : {H_mpo.expectation_value(psi=psii):.16f}')
+    print(f'Lanczos-step energy : {E_alpha:.16f}')
 
-    print(f'h1 : {h1:.16f}')    
-    print(f'Ei : {H_mpo.expectation_value(psi=psii):.16f}')
 
-    # Compute the Lanczos-optimized energy using the estimated moments and the optimal alpha
+plot_E_vs_chi(
+    chi_maxs,
+    E_dmrg,
+    E_lanczos,
+    E_exact,
+    filename='energy_vs_chi_focus1',)
 
-    # print(f'Raw optimization: {h1:.16f}')
-    print(f'Optimized estimated: {E_alpha:.16f}')
-    # print(f'Exact diagonalization: {E_exact:.16f}')
-    # print(f'Difference between raw and optimized: {h1 - E_alpha:.16f}')
 
-# 1. Measure the exact particle number of your DMRG state
-N_dmrg = np.sum(psii.expectation_value('N'))
-print(f"DMRG Total Particles: {N_dmrg:.2f}")
-
-# 2. Check the charge sector of your Exact Diagonalization state
-# Assuming your ED state was returned as `psi_exact_array`
-# ed_charge = psi_exact.qtotal
-# print(f"ED Ground State Charge Sector: {ed_charge}")
