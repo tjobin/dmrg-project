8937dba84dc51db9aaa5f9baaa20eb6d481ecc04
Timothé Jobin <timothe.jobin@epfl.ch>
2026-03-23 15:38:21 +0100

    added direct calculation of analytical solutions

--- lanczos_method.py
+++ lanczos_method.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def optimize_lanczos_step_analytical(
+def optimize_lanczos_step(
         h1: float,
         h2: float,
         h3: float,
@@ -13,9 +13,9 @@ def optimize_lanczos_step_analytical(
         h2: float, the second moment of the Hamiltonian.
         h3: float, the third moment of the Hamiltonian.
     Returns:
-        alpha_opt: float, the optimal variational parameter.
+        alpha_p: float, first anayltical solution.
+        alpha_m: float, second analytical solution
     """    
     alpha_p = (-(h3 - h1*h2) + np.sqrt((h3-h1*h2)**2 - 4*(h2-h1**2)*(h1*h3-h2**2))) / (2*(h1*h3-h2**2))
     alpha_m = (-(h3 - h1*h2) - np.sqrt((h3-h1*h2)**2 - 4*(h2-h1**2)*(h1*h3-h2**2))) / (2*(h1*h3-h2**2))
-    alpha_opt = min(alpha_p, alpha_m)
-    return alpha_opt
+    return alpha_p, alpha_m
