c343ecf0d997e23cac7fc3eec0bba57208430af7
Timothé Jobin <timothe.jobin@epfl.ch>
2026-03-23 15:39:44 +0100

    add variance_vs_sample for different cutoffs

--- _plot.py
+++ _plot.py
@@ -5,7 +5,7 @@ def plot_E_vs_chi(
         chi: list[int],
         E: list[float],
         E_alpha: list[float],
-        E_exact: float,
+        E_exact: float | None = None,
         dim: int | list[int] = 1,
         filename: str = 'energy_vs_chi'
         ) -> None:
@@ -15,10 +15,23 @@ def plot_E_vs_chi(
     plt.figure(figsize=(10, 6))
     plt.plot(chi, E, marker='o', linestyle='--', color='tab:blue', label='DMRG')
     plt.plot(chi, E_alpha, marker='s', linestyle='-.', color='tab:orange', label='DMRG + Lanczos')
-    plt.axhline(E_exact, color='k', linestyle='-', label='Exact')
+    if E_exact is not None:
+        plt.axhline(E_exact, color='k', linestyle='-', label='Exact')
     plt.title(f'Energy vs. Chi (L={dim})')
     plt.xlabel('Chi')
     plt.ylabel('Energy (Ha)')
     plt.grid()
     plt.legend()
     plt.savefig(f'figs/{filename}', bbox_inches='tight')
+
+def plot_variance_vs_samples(
+        vars_per_c: list,
+        samples: list,
+        cs: list,
+        filename: str = 'variance_vs_samples'
+    ):
+    plt.figure()
+    for i, vars in enumerate(vars_per_c):
+        plt.plot(samples, vars, marker='o', linestyle='--', label=f'c={cs[i]}')
+    plt.legend()
+    plt.savefig(f'figs/{filename}.png', bbox_inches='tight')
