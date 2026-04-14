beae7748187e1a377335f9b2d828918eec7e3c7d
Timothé Jobin <timothe.jobin@epfl.ch>
2026-04-14 10:52:16 +0200

    feature: handles negative square roots (due to sampling noise)

--- lanczos_method.py
+++ lanczos_method.py
@@ -16,6 +16,19 @@ def optimize_lanczos_step(
         alpha_p: float, first anayltical solution.
         alpha_m: float, second analytical solution
     """    
-    alpha_p = (-(h3 - h1*h2) + np.sqrt((h3-h1*h2)**2 - 4*(h2-h1**2)*(h1*h3-h2**2))) / (2*(h1*h3-h2**2))
-    alpha_m = (-(h3 - h1*h2) - np.sqrt((h3-h1*h2)**2 - 4*(h2-h1**2)*(h1*h3-h2**2))) / (2*(h1*h3-h2**2))
+    if (h3-h1*h2)**2 - 4*(h2-h1**2)*(h1*h3-h2**2) <= 0:
+        print("Warning: negative square root; setting it to 0.")
+        alpha_p = (-(h3 - h1*h2)) / (2*(h1*h3-h2**2))
+        alpha_m = alpha_p
+    else:
+        alpha_p = (-(h3 - h1*h2) + np.sqrt((h3-h1*h2)**2 - 4*(h2-h1**2)*(h1*h3-h2**2))) / (2*(h1*h3-h2**2))
+        alpha_m = (-(h3 - h1*h2) - np.sqrt((h3-h1*h2)**2 - 4*(h2-h1**2)*(h1*h3-h2**2))) / (2*(h1*h3-h2**2))
+    if alpha_p > 10**12:
+        alpha_p = 10**12
+    elif alpha_p < -10**12:
+        alpha_p = -10**12
+    if alpha_m > 10**12:
+        alpha_m = 10**12
+    elif alpha_m < -10**12:
+        alpha_m = -10**12
     return alpha_p, alpha_m
