52378a0c2430d39ee9381d1b33a5ab5da55674e8
Timothé Jobin <timothe.jobin@epfl.ch>
2026-04-17 12:18:46 +0200

    feature: add comprehensive function to do lanczos step

--- lanczos_method.py
+++ lanczos_method.py
@@ -1,6 +1,11 @@
 import numpy as np
+from tenpy.networks.mps import MPS
+from tenpy.networks.mpo import MPO
+from tqdm import tqdm
+from moments_estimator import estimate_hamiltonian_moments_cheap, get_mpo_moments_bruteforce
 
-def optimize_lanczos_step(
+
+def get_optimized_alphas(
         h1: float,
         h2: float,
         h3: float,
@@ -21,8 +26,8 @@ def optimize_lanczos_step(
         alpha_p = (-(h3 - h1*h2)) / (2*(h1*h3-h2**2))
         alpha_m = alpha_p
     else:
-        alpha_p = (-(h3 - h1*h2) + np.sqrt((h3-h1*h2)**2 - 4*(h2-h1**2)*(h1*h3-h2**2))) / (2*(h1*h3-h2**2))
-        alpha_m = (-(h3 - h1*h2) - np.sqrt((h3-h1*h2)**2 - 4*(h2-h1**2)*(h1*h3-h2**2))) / (2*(h1*h3-h2**2))
+        alpha_p = (-(h3 - h1*h2) + np.sqrt((h3-h1*h2)**2 - 4*(h2-h1**2)*(h1*h3-h2**2))) / (2*(h1*h3-h2**2) + 1e-12)
+        alpha_m = (-(h3 - h1*h2) - np.sqrt((h3-h1*h2)**2 - 4*(h2-h1**2)*(h1*h3-h2**2))) / (2*(h1*h3-h2**2) + 1e-12)
     if alpha_p > 10**12:
         alpha_p = 10**12
     elif alpha_p < -10**12:
@@ -32,3 +37,71 @@ def optimize_lanczos_step(
     elif alpha_m < -10**12:
         alpha_m = -10**12
     return alpha_p, alpha_m
+
+def lanczos_step_sampled(
+        psi: MPS,
+        H: MPO,
+        N_s: int,
+        chi_max: int,
+        seed: int = None,
+        filename: str = None):
+    
+    h1, h2, h3 = estimate_hamiltonian_moments_cheap(
+        psi = psi,
+        H = H,
+        N_s = N_s,
+        chi_max = chi_max,
+        seed = seed,
+        filename = filename
+    )
+
+    alpha_p, alpha_m = get_optimized_alphas(h1, h2, h3)
+    phi_1 = psi.copy()
+    H.apply_naively(phi_1)
+
+    psi_alpha_p = psi.add(other=phi_1, alpha=1.0, beta=alpha_p)
+    psi_alpha_m = psi.add(other=phi_1, alpha=1.0, beta=alpha_m)
+
+    E_alpha_p = np.real(H.expectation_value(psi=psi_alpha_p))
+    E_alpha_m = np.real(H.expectation_value(psi=psi_alpha_m))
+
+    if E_alpha_p < E_alpha_m:
+        E_alpha = E_alpha_p
+        alpha_star = alpha_p
+        psi_alpha = psi_alpha_p
+    else:
+        E_alpha = E_alpha_m
+        alpha_star = alpha_m
+        psi_alpha = psi_alpha_m
+
+    return E_alpha, psi_alpha, alpha_star
+
+def lanczos_step_exact(
+        psi: MPS,
+        H: MPO,):
+    
+    h1_exact, h2_exact, h3_exact = get_mpo_moments_bruteforce(
+        psi = psi,
+        H = H,
+    )
+
+    alpha_p, alpha_m = get_optimized_alphas(h1_exact, h2_exact, h3_exact)
+    phi_1 = psi.copy()
+    H.apply_naively(phi_1)
+
+    psi_alpha_p = psi.add(other=phi_1, alpha=1.0, beta=alpha_p)
+    psi_alpha_m = psi.add(other=phi_1, alpha=1.0, beta=alpha_m)
+
+    E_alpha_p = np.real(H.expectation_value(psi=psi_alpha_p))
+    E_alpha_m = np.real(H.expectation_value(psi=psi_alpha_m))
+
+    if E_alpha_p < E_alpha_m:
+        E_alpha_exact = E_alpha_p
+        alpha_star_exact = alpha_p
+        psi_alpha_exact = psi_alpha_p
+    else:
+        E_alpha_exact = E_alpha_m
+        alpha_star_exact = alpha_m
+        psi_alpha_exact = psi_alpha_m
+
+    return E_alpha_exact, psi_alpha_exact, alpha_star_exact
